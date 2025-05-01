f397a40d2ed1f9510504546b1d9626f35a8fd920
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-05-02 02:30:59 +0800

    refactor: remove references to provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
-// NOTE: packages
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-// NOTE: firebase imports
 import 'package:firebase_core/firebase_core.dart';
 import './firebase_options.dart';
-/////
-import '../providers/user_auth_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: ((context) => UserAuthProvider())),
-      ],
-      child: const MainApp(),
-    ),
-  );
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -26,11 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
