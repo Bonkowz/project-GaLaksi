3d69baf4b3bdc30ad9fbfaf93402c978a6736b20
Alan Vender <asvender@gmail.com>
2025-05-02 13:36:27 +0800

    chore: added model itinerary and user model files

--- lib/providers/user_auth_provider.dart
+++ lib/providers/user_auth_provider.dart
@@ -3,7 +3,5 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../apis/firebase_auth._api.dart';
 
 class UserAuthProvider with ChangeNotifier {
-  UserAuthProvider () {
-    
-  }
+  UserAuthProvider();
 }
