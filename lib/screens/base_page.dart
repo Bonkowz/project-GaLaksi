009d59d3f972ad4ffce2fe78ea54c5c52c145fea
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-07 13:23:29 +0800

    feat: Add non-functional Bottom App Bar

--- lib/screens/base_page.dart
+++ lib/screens/base_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:galaksi/screens/main/find_people_page.dart';
+import 'package:galaksi/screens/main/home_page.dart';
+import 'package:galaksi/screens/main/my_friends_page.dart';
+import 'package:galaksi/screens/main/profile_page.dart';
 
 /// [BasePage] will act as a Parent of the following pages through a [BottomNavigationBar]:
-///
+/// [HomePage], [FindPeoplePage], [ProfilePage], [MyFriendsPage]
 class BasePage extends StatefulWidget {
   const BasePage({super.key});
 
@@ -11,11 +14,48 @@ class BasePage extends StatefulWidget {
 }
 
 class _BasePageState extends State<BasePage> {
+  final List<Widget> _pages = [
+    const HomePage(),
+    const FindPeoplePage(),
+    const ProfilePage(),
+    const MyFriendsPage(),
+  ];
+
+  final List<Widget> _navigationButtons = [
+    IconButton(onPressed: () => {}, icon: const Icon(Icons.home)),
+    IconButton(onPressed: () => {}, icon: const Icon(Icons.person_search)),
+    const SizedBox(width: 50), // Spacing
+    IconButton(onPressed: () => {}, icon: const Icon(Icons.person)),
+    IconButton(onPressed: () => {}, icon: const Icon(Icons.people)),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: []),
-      body: Center(child: Text("Find People Page")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        enableFeedback: true,
+        child: Icon(
+          Icons.add_location_alt,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        onPressed: () {},
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        shape: const CircleBorder(),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8,
+        elevation: 100, // Slight lift
+        shadowColor: Colors.black,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _navigationButtons,
+        ),
+        color: Theme.of(context).colorScheme.surfaceContainer,
+      ),
+      body: Center(child: Text("Base Page")),
     );
   }
 }
