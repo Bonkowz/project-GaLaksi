5e482a06e81828adb94e35b637c116cf76c9be7d
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-05-07 17:43:43 +0800

    refactor: improve formatting of itinerary_model.dart and user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,17 +1,16 @@
 import 'dart:convert';
 
 class User {
-  String? id;
-  String firstName;
-  String lastName;
-  String userName;
-  String phoneNumber;
-  String email;
-  List<String>? interests;
-  List<String>? travelStyles;
-
-
-  User({this.id, required this.firstName, required this.lastName, required this.userName, required this.phoneNumber, required this.email, this.interests, this.travelStyles});
+  User({
+    required this.firstName,
+    required this.lastName,
+    required this.userName,
+    required this.phoneNumber,
+    required this.email,
+    this.id,
+    this.interests,
+    this.travelStyles,
+  });
 
   // Factory constructor to instantiate object from json format
   factory User.fromJson(Map<String, dynamic> json) {
@@ -27,12 +26,29 @@ class User {
     );
   }
 
+  String? id;
+  String firstName;
+  String lastName;
+  String userName;
+  String phoneNumber;
+  String email;
+  List<String>? interests;
+  List<String>? travelStyles;
+
   static List<User> fromJsonArray(String jsonData) {
     final Iterable<dynamic> data = jsonDecode(jsonData);
     return data.map<User>((dynamic d) => User.fromJson(d)).toList();
   }
 
   Map<String, dynamic> toJson() {
-    return {'firstName': firstName, 'lastName': lastName, 'userName': userName, 'phoneNumber': phoneNumber, 'email': email, 'interests': interests, 'travelStyles': travelStyles};
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'userName': userName,
+      'phoneNumber': phoneNumber,
+      'email': email,
+      'interests': interests,
+      'travelStyles': travelStyles,
+    };
   }
 }
