8b56a0b937fbf5aedd84b3a4b695a99d4fa1d6e1
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-05-10 22:58:09 +0800

    feat: add TravelStyle model

---
+++ lib/models/travel_style_model.dart
@@ -0,0 +1,12 @@
+enum TravelStyle {
+  beachVacations("Beach Vacations"),
+  businessTrips("Business Trips"),
+  cityTours("City Tours"),
+  fineDining("Fine Dining"),
+  natureTreks("Nature Treks"),
+  roadTrips("Road Trips"),
+  staycations("Staycations");
+
+  const TravelStyle(this.title);
+  final String title;
+}
