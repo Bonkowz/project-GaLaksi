6af0ce1c45274931a34a32ba403c144e4d2d48dd
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-05-10 23:39:47 +0800

    feat: add firestore api and update User model
    The user model is now updated to have a uid referencing the Auth user's uid.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,54 +1,49 @@
-import 'dart:convert';
+import 'package:galaksi/models/interest_model.dart';
+import 'package:galaksi/models/travel_style_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
   User({
     required this.firstName,
     required this.lastName,
-    required this.userName,
-    required this.phoneNumber,
+    required this.username,
     required this.email,
-    this.id,
-    this.interests,
-    this.travelStyles,
+    required this.uid,
+    required this.interests,
+    required this.travelStyles,
   });
 
-  // Factory constructor to instantiate object from json format
-  factory User.fromJson(Map<String, dynamic> json) {
+  /// Factory constructor to instantiate object from a [DocumentSnapshot]
+  factory User.fromDocument(DocumentSnapshot doc) {
+    final map = doc.data() as Map<String, dynamic>;
     return User(
-      id: json['id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      userName: json['userName'],
-      phoneNumber: json['phoneNumber'],
-      email: json['email'],
-      interests: json['interests'],
-      travelStyles: json['travelStyles'],
+      // The uid is directly obtainable from the document id, since they match
+      uid: doc.id,
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      username: map['username'],
+      email: map['email'],
+      interests: map['interests'],
+      travelStyles: map['travelStyles'],
     );
   }
 
-  String? id;
+  String uid;
   String firstName;
   String lastName;
-  String userName;
-  String phoneNumber;
+  String username;
   String email;
-  List<String>? interests;
-  List<String>? travelStyles;
+  Set<Interest>? interests;
+  Set<TravelStyle>? travelStyles;
 
-  static List<User> fromJsonArray(String jsonData) {
-    final Iterable<dynamic> data = jsonDecode(jsonData);
-    return data.map<User>((dynamic d) => User.fromJson(d)).toList();
-  }
-
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toMap() {
     return {
       'firstName': firstName,
       'lastName': lastName,
-      'userName': userName,
-      'phoneNumber': phoneNumber,
+      'username': username,
       'email': email,
-      'interests': interests,
-      'travelStyles': travelStyles,
+      'interests': interests?.map((i) => i.name).toList(),
+      'travelStyles': travelStyles?.map((t) => t.name).toList(),
     };
   }
 }
