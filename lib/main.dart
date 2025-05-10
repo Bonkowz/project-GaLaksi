4cb011cd847df720932de7b7fe708a80aae42466
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-05-10 23:41:27 +0800

    refactor: removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:galaksi/providers/auth/auth_notifier.dart';
 import 'package:galaksi/screens/auth/auth_screen.dart';
-import 'package:galaksi/screens/auth/sign_out_page.dart';
 import 'package:galaksi/screens/base_page.dart';
 import 'package:galaksi/theme/theme.dart';
 import 'package:galaksi/theme/util.dart';
