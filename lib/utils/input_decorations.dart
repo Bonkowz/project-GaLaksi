25f8e698f653d057c3ceb5d1241f318b8a01ec10
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-13 23:33:07 +0800

    feat: Add underline input border

--- lib/utils/input_decorations.dart
+++ lib/utils/input_decorations.dart
@@ -30,4 +30,34 @@ abstract class InputDecorations {
       contentPadding: contentPadding,
     );
   }
+
+  static InputDecoration underlineBorder({
+    required BuildContext context,
+    String? labelText,
+    String? hintText,
+    String? errorText,
+    String? helperText,
+    Widget? prefixIcon,
+    Widget? suffixIcon,
+    Color? borderColor,
+    double? borderRadius,
+    EdgeInsetsGeometry? contentPadding,
+  }) {
+    return InputDecoration(
+      border: UnderlineInputBorder(
+        borderSide: BorderSide(color: borderColor ?? const Color(0xFF000000)),
+        borderRadius:
+            borderRadius == null
+                ? const BorderRadius.all(Radius.circular(0))
+                : BorderRadius.all(Radius.circular(borderRadius)),
+      ),
+      labelText: labelText,
+      hintText: hintText,
+      prefixIcon: prefixIcon,
+      suffixIcon: suffixIcon,
+      errorText: errorText,
+      helperText: helperText ?? "",
+      contentPadding: contentPadding,
+    );
+  }
 }
