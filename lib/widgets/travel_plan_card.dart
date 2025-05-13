58d0e745bd97f661c225da2d3af1afcd5ac876aa
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-14 04:33:02 +0800

    ui: Add placeholder layout for future functionality in Travel Plan Card

--- lib/widgets/travel_plan_card.dart
+++ lib/widgets/travel_plan_card.dart
@@ -1,26 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:galaksi/models/travel_plan/travel_plan_model.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class TravelPlanCard extends StatelessWidget {
-  const TravelPlanCard(this.travelPlan, {super.key});
+  const TravelPlanCard({required this.travelPlan, super.key});
 
   final TravelPlan travelPlan;
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Container(
       margin:
           EdgeInsets.zero +
-          EdgeInsets.only(top: 8) +
-          EdgeInsets.only(bottom: 8),
-      height: 200,
+          const EdgeInsets.only(top: 8) +
+          const EdgeInsets.only(bottom: 8),
+      height: 180,
       child: Card.outlined(
         margin: EdgeInsets.zero,
-        child: Column(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(travelPlan.title),
-            Text(travelPlan.description),
-            Text("${travelPlan.activities.length} activities"),
+            Expanded(
+              flex: 4,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Image(
+                  image: AssetImage('assets/images/galaksi-placeholder.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  spacing: 4,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4),
+                      child: Text(
+                        travelPlan.title,
+                        style: textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    Row(
+                      spacing: 4,
+                      children: [
+                        Icon(Icons.alarm),
+                        Text(
+                          "Happening in 2 days",
+                          style: textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      spacing: 4,
+                      children: [
+                        Icon(Icons.calendar_month),
+                        Text(
+                          "May 5 - June 3, 2025",
+                          style: textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      spacing: 4,
+                      children: [
+                        Icon(Icons.map),
+                        Text(
+                          travelPlan.activities.length == 0
+                              ? "No activities yet"
+                              : "${travelPlan.activities.length} activities",
+                          style: textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
