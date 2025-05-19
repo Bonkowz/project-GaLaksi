02841cbf1ec86a8a4dabe9375ff93d929358612c
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-20 00:16:51 +0800

    feat: Add single travel plan stream provider

--- lib/providers/travel_plan/get_travel_plan_provider.dart
+++ lib/providers/travel_plan/get_travel_plan_provider.dart
@@ -42,3 +42,39 @@ Stream<List<TravelPlan>> myTravelPlansStream(Ref ref) {
     },
   );
 }
+
+@riverpod
+Stream<TravelPlan?> travelPlanStream(Ref ref, String travelPlanId) {
+  final currentAuthUser = ref.watch(currentUserStreamProvider);
+  final authUid = currentAuthUser.when(
+    data: (user) => user?.uid,
+    error: (error, stackTrace) {
+      debugPrint("Error fetching current user: $stackTrace");
+      return null;
+    },
+    loading: () {
+      debugPrint("Auth data is loading.");
+      return null;
+    },
+  );
+
+  // If the UID is null, we cannot create a profile
+  if (authUid == null) {
+    debugPrint("Error fetching travel plans");
+    return const Stream.empty();
+  }
+
+  final result = FirebaseFirestoreApi().fetchTravelPlan(travelPlanId);
+
+  return result.when(
+    onSuccess: (success) {
+      return success.data.map(
+        (doc) => doc.exists ? TravelPlan.fromDocument(doc) : null,
+      );
+    },
+    onFailure: (failure) {
+      debugPrint("Error fetching data: ${failure.message}");
+      return const Stream.empty();
+    },
+  );
+}
--- lib/providers/travel_plan/get_travel_plan_provider.g.dart
+++ lib/providers/travel_plan/get_travel_plan_provider.g.dart
@@ -25,5 +25,148 @@ final myTravelPlansStreamProvider = StreamProvider<List<TravelPlan>>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef MyTravelPlansStreamRef = StreamProviderRef<List<TravelPlan>>;
+String _$travelPlanStreamHash() => r'500062c3e5355edaaad12a800ea98bcad59fbc4f';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [travelPlanStream].
+@ProviderFor(travelPlanStream)
+const travelPlanStreamProvider = TravelPlanStreamFamily();
+
+/// See also [travelPlanStream].
+class TravelPlanStreamFamily extends Family<AsyncValue<TravelPlan?>> {
+  /// See also [travelPlanStream].
+  const TravelPlanStreamFamily();
+
+  /// See also [travelPlanStream].
+  TravelPlanStreamProvider call(String travelPlanId) {
+    return TravelPlanStreamProvider(travelPlanId);
+  }
+
+  @override
+  TravelPlanStreamProvider getProviderOverride(
+    covariant TravelPlanStreamProvider provider,
+  ) {
+    return call(provider.travelPlanId);
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'travelPlanStreamProvider';
+}
+
+/// See also [travelPlanStream].
+class TravelPlanStreamProvider extends AutoDisposeStreamProvider<TravelPlan?> {
+  /// See also [travelPlanStream].
+  TravelPlanStreamProvider(String travelPlanId)
+    : this._internal(
+        (ref) => travelPlanStream(ref as TravelPlanStreamRef, travelPlanId),
+        from: travelPlanStreamProvider,
+        name: r'travelPlanStreamProvider',
+        debugGetCreateSourceHash:
+            const bool.fromEnvironment('dart.vm.product')
+                ? null
+                : _$travelPlanStreamHash,
+        dependencies: TravelPlanStreamFamily._dependencies,
+        allTransitiveDependencies:
+            TravelPlanStreamFamily._allTransitiveDependencies,
+        travelPlanId: travelPlanId,
+      );
+
+  TravelPlanStreamProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.travelPlanId,
+  }) : super.internal();
+
+  final String travelPlanId;
+
+  @override
+  Override overrideWith(
+    Stream<TravelPlan?> Function(TravelPlanStreamRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: TravelPlanStreamProvider._internal(
+        (ref) => create(ref as TravelPlanStreamRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        travelPlanId: travelPlanId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeStreamProviderElement<TravelPlan?> createElement() {
+    return _TravelPlanStreamProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is TravelPlanStreamProvider &&
+        other.travelPlanId == travelPlanId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, travelPlanId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin TravelPlanStreamRef on AutoDisposeStreamProviderRef<TravelPlan?> {
+  /// The parameter `travelPlanId` of this provider.
+  String get travelPlanId;
+}
+
+class _TravelPlanStreamProviderElement
+    extends AutoDisposeStreamProviderElement<TravelPlan?>
+    with TravelPlanStreamRef {
+  _TravelPlanStreamProviderElement(super.provider);
+
+  @override
+  String get travelPlanId => (origin as TravelPlanStreamProvider).travelPlanId;
+}
+
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
