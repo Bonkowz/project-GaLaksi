f43b1bc52574f12bf7bcb9d1df8977e7478263dc
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-20 03:20:34 +0800

    feat: Add navigateTo property

--- lib/widgets/create_details_button.dart
+++ lib/widgets/create_details_button.dart
@@ -7,23 +7,32 @@ class CreateDetailsButton extends StatelessWidget {
     required this.text,
     required this.leadingIcon,
     required this.trailingIcon,
+    required this.navigateTo,
   });
 
   String text;
   Icon leadingIcon;
   Icon trailingIcon;
+  Widget navigateTo;
 
   @override
   Widget build(BuildContext context) {
-    return Card.outlined(
-      child: SizedBox(
-        width: double.infinity,
-        child: Row(
-          children: [
-            Padding(padding: const EdgeInsets.all(24.0), child: leadingIcon),
-            Expanded(child: Text(text)),
-            Padding(padding: const EdgeInsets.all(24.0), child: trailingIcon),
-          ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(
+          context,
+        ).push(MaterialPageRoute(builder: (context) => navigateTo));
+      },
+      child: Card.outlined(
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Padding(padding: const EdgeInsets.all(24.0), child: leadingIcon),
+              Expanded(child: Text(text)),
+              Padding(padding: const EdgeInsets.all(24.0), child: trailingIcon),
+            ],
+          ),
         ),
       ),
     );
