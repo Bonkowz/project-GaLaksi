7dedc6422e368eddf900888cd46a88fab920111f
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-20 03:02:58 +0800

    fix: Add Expanded to prevent overflow

--- lib/screens/travel_details/itinerary_tab.dart
+++ lib/screens/travel_details/itinerary_tab.dart
@@ -12,6 +12,16 @@ class ItineraryTab extends StatelessWidget {
       "Quick camping and breakfast at basecamp",
     ],
     [
+      "7:15 AM to 8:15 AM",
+      "Mount Everest Basecamp",
+      "Quick camping and breakfast at basecamp",
+    ],
+    [
+      "7:15 AM to 8:15 AM",
+      "Mount Everest Basecamp",
+      "Quick camping and breakfast at basecamp",
+    ],
+    [
       "8:15 AM to 10:00 AM",
       "Mount Everest Camp IV",
       "Go to camp IV and prepare for summit",
@@ -29,43 +39,45 @@ class ItineraryTab extends StatelessWidget {
           leadingIcon: const Icon(Symbols.add),
           trailingIcon: const Icon(Symbols.map),
         ),
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: tempItineraryItems.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tempItineraryItems[index][0], // Itinerary item text
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          tempItineraryItems[index][1], // Itinerary item text
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        Text(
-                          tempItineraryItems[index][2], // Itinerary item text
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ],
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: tempItineraryItems.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tempItineraryItems[index][0], // Itinerary item text
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            tempItineraryItems[index][1], // Itinerary item text
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Text(
+                            tempItineraryItems[index][2], // Itinerary item text
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ],
     );
