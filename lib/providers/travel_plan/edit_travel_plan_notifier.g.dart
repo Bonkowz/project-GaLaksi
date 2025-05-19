0c73ea2f8e108f637ea4ba238d52098c551efe6c
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-20 00:00:53 +0800

    feat: Add EditTravelPlanNotifier provider

---
+++ lib/providers/travel_plan/edit_travel_plan_notifier.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_travel_plan_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$editTravelPlanNotifierHash() =>
+    r'3c1423ab4ba11c439a6690780271a2a449666de8';
+
+/// See also [EditTravelPlanNotifier].
+@ProviderFor(EditTravelPlanNotifier)
+final editTravelPlanNotifierProvider = AutoDisposeNotifierProvider<
+  EditTravelPlanNotifier,
+  TravelPlanState
+>.internal(
+  EditTravelPlanNotifier.new,
+  name: r'editTravelPlanNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$editTravelPlanNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EditTravelPlanNotifier = AutoDisposeNotifier<TravelPlanState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
