bec4e0bdf92f0d52227bbb59c0dfd72bbdd6d43b
Euan Jed Tabamo <estabamo1@up.edu.ph>
2025-05-20 12:59:13 +0800

    feat: add a new notifier that for user doc updates in the background

---
+++ lib/providers/user_profile/user_profile_notifier.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_profile_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currentUserProfileStreamHash() =>
+    r'a82bb9975f3f3d35a87422c416c02e1c2928bcdd';
+
+/// See also [currentUserProfileStream].
+@ProviderFor(currentUserProfileStream)
+final currentUserProfileStreamProvider =
+    AutoDisposeStreamProvider<User?>.internal(
+      currentUserProfileStream,
+      name: r'currentUserProfileStreamProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$currentUserProfileStreamHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CurrentUserProfileStreamRef = AutoDisposeStreamProviderRef<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
