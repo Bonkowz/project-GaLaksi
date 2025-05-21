7261b914f38b5dbdc65379c80e78e9dde245744f
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-22 04:22:09 +0800

    chore: Code comments

--- lib/providers/http_client/http_client_provider.dart
+++ lib/providers/http_client/http_client_provider.dart
@@ -2,8 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'http_client_provider.g.dart'; // required for code generation
+part 'http_client_provider.g.dart';
 
+// Provider for a global HTTP client state
 @riverpod
 http.Client httpClient(Ref ref) {
   final client = http.Client();
