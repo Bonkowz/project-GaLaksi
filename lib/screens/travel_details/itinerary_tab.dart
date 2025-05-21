a1eda8a7b2e44c3844f234866ba67534ae07bf07
Alan Vender <asvender@gmail.com>
2025-05-22 02:06:21 +0800

    chore: added dummy files to accom,flight,notes,iten chore: added pages tabs for iten, flight

--- lib/screens/travel_details/itinerary_tab.dart
+++ lib/screens/travel_details/itinerary_tab.dart
@@ -7,6 +7,43 @@ import 'package:material_symbols_icons/symbols.dart';
 class ItineraryTab extends StatelessWidget {
   ItineraryTab({required this.activities, super.key});
   List<TravelActivity> activities;
+  List <TravelActivity> activitiesTemp = [
+    TravelActivity(
+      startAt: DateTime(2025, 5, 5, 7, 15),
+      endAt: DateTime(2025, 5, 5, 8, 15),
+      title: "Quick camping and breakfast",
+      location: "Mount Everest Basecamp",
+      reminders: [Duration(minutes: 60)],
+    ),
+    TravelActivity(
+      startAt: DateTime(2025, 5, 5, 8, 15),
+      endAt: DateTime(2025, 5, 5, 10, 0),
+      title: "Prepare for summit push",
+      location: "Mount Everest Camp IV",
+      reminders: [Duration(minutes: 30)],
+    ),
+    TravelActivity(
+      startAt: DateTime(2025, 5, 5, 10, 0),
+      endAt: DateTime(2025, 5, 5, 19, 0),
+      title: "Summit Push",
+      location: "Mount Everest Summit",
+      reminders: [],
+    ),
+    TravelActivity(
+      startAt: DateTime(2025, 5, 5, 19, 0),
+      endAt: DateTime(2025, 6, 6, 6, 0),
+      title: "Sleep",
+      location: "Mount Everest Summit",
+      reminders: [],
+    ),
+    TravelActivity(
+      startAt: DateTime(2025, 5, 6, 7, 0),
+      endAt: DateTime(2025, 5, 6, 12, 0),
+      title: "Descent to Camp IV",
+      location: "Mount Everest Camp IV",
+      reminders: [Duration(minutes: 120)],
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -21,14 +58,12 @@ class ItineraryTab extends StatelessWidget {
           child: ListView.builder(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemCount: activities.length,
+            itemCount: activitiesTemp.length,
             itemBuilder: (context, index) {
               // TODO: add documentation here
-              final activity = activities[index];
-              final startTime =
-                  "${activity.startAt.toLocal().hour}:${activity.startAt.toLocal().minute.toString().padLeft(2, '0')}";
-              final endTime =
-                  "${activity.endAt.toLocal().hour}:${activity.endAt.toLocal().minute.toString().padLeft(2, '0')}";
+              final activity = activitiesTemp[index];
+              final startTime = "${activity.startAt.toLocal().hour}:${activity.startAt.toLocal().minute.toString().padLeft(2, '0')}";
+              final endTime = "${activity.endAt.toLocal().hour}:${activity.endAt.toLocal().minute.toString().padLeft(2, '0')}";
               final timeRange = "$startTime to $endTime";
 
               return Padding(
