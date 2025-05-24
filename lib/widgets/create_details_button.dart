95ccfe3e22d4a4102fd3d14e0cb734bccad7b515
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-24 16:10:10 +0800

    ui: Change color of add details

--- lib/widgets/create_details_button.dart
+++ lib/widgets/create_details_button.dart
@@ -17,7 +17,7 @@ class CreateDetailsButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card.outlined(
-      color: Theme.of(context).colorScheme.surfaceContainerLow,
+      color: Theme.of(context).colorScheme.surfaceContainerLowest,
       child: InkWell(
         onTap: () {
           Navigator.of(
