81125774e58dc6ec46a22a52baab256ebe0eaedf
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-25 20:49:00 +0800

    feat: Add logout modal

---
+++ lib/screens/overlays/logout_modal.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:galaksi/providers/auth/auth_notifier.dart';
+
+class LogoutModal extends ConsumerWidget {
+  const LogoutModal({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Material(
+        borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Logout', style: Theme.of(context).textTheme.titleLarge),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                'Are you sure you want to log out?',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      ref.read(authNotifierProvider.notifier).signOut();
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Log out',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Never mind',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
