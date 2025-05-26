9a65b59107696f543cb087a2cdef13191ac7ca20
jcmagpantay <jcmagpantay@up.edu.ph>
2025-05-26 09:38:38 +0800

    chore: Add utils for time printing

--- lib/screens/main/notifications_page.dart
+++ lib/screens/main/notifications_page.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:galaksi/services/notification_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:galaksi/providers/notifications/notification_service_provider.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends ConsumerWidget {
   const NotificationsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final notificationService = ref.read(notificationServiceProvider);
     return Center(
       child: TextButton(
         onPressed: () {
-          NotificationService().scheduleNotification(
+          notificationService.scheduleNotification(
             title: "Yeah Boit",
             body: "Hello",
             scheduledDate: DateTime.now().add(Duration(seconds: 5)),
